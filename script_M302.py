8576b5183a4dccffee3c58c7d662a3b1873c765d
Eduardo Ruiz <eduardoruiz.pedre@hotmail.com>
2020-01-15 16:31:32 +0000

    first commit

---
+++ script_M302.py
@@ -0,0 +1,15 @@
+import xml.etree.ElementTree as ET
+import requests
+import json
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+headers = {'accept': 'application/json'}
+r = requests.get('https://zenodo.org/api/records/3608959',headers)
+record = json.loads(r.text)
+url = record['files'][0]["links"]["self"]
+data = pd.read_csv(url)
+
+#plt.figure(figsize=(10,8))
+#plt.scatter(x=data.X,y=data.Y,s=data.Poblacion/10000)
